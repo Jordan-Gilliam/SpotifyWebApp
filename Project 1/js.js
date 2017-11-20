ae591a3abf29ce27cc8c4c101d7fd7cfdd3bc5a3
isabella ambrosio <isa.oambrosio@gmail.com>
2017-11-20 15:26:40 +0000

    layout

---
+++ Project 1/js.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+
+    // Initialize collapse button
+    $(".button-collapse").sideNav();
+
+
+    $(document).ready(function() {
+        $('.carousel').carousel();
+    });
+
+});
